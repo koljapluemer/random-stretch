b862ad50def19c865568bc38f3a063398816c50e
koljapluemer <github@koljapluemer.com>
2025-04-03 14:03:50 +0200

    improve layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="flex gap-2 flex-col items-center p-2">
+  <header class="flex gap-2 flex-col md:flex-row md:justify-between items-center py-2 border-b-1">
     <router-link class="font-bold text-lg" to="/">Random Stretch Recommender</router-link>
     <nav class="flex flex-row gap-2">
       <!-- make btn-primary is path is exactly "/main" -->
@@ -15,32 +15,26 @@
         Desk</router-link>
     </nav>
   </header>
-  <main class="flex flex-col gap-4 flex-center p-10 max-w-lg mx-auto w-full">
+  <main class="flex flex-col gap-4 py-10 mx-auto w-full">
     <router-view :key="$route.fullPath" />
   </main>
 
-  <footer class="border-t-2 mt-10 w-full p-4 text-sm">
-    <div id="socials" class="mb-4 flex justify-center gap-2">
-      <!-- Twitter Share Button -->
-      <a href="https://twitter.com/share?url=https://stretch.koljapluemer.com/" target="_blank" rel="noopener">
-        <i class="fab fa-twitter share-icon"></i> Share on Twitter/X
-      </a>
+  <footer class="border-t-2 mt-10 w-full py-4 text-sm flex flex-col md:flex-row justify-center gap-2">
+    <!-- Twitter Share Button -->
+    <a href="https://twitter.com/share?url=https://stretch.koljapluemer.com/" target="_blank" rel="noopener" class="link">
+      <i class="fab fa-twitter share-icon"></i>→ Share on Twitter/X
+    </a>
 
-      <!-- Reddit Share Button -->
-      <a href="https://www.reddit.com/submit?url=https://stretch.koljapluemer.com/" target="_blank" rel="noopener">
-        <i class="fab fa-reddit share-icon"></i> Share on Reddit
-      </a>
+    <!-- Reddit Share Button -->
+    <a href="https://www.reddit.com/submit?url=https://stretch.koljapluemer.com/" target="_blank" rel="noopener" class="link">
+      <i class="fab fa-reddit share-icon"></i>→ Share on Reddit
+    </a>
 
-    </div>
 
-    <div class="mb-4 flex justify-center gap-2 items-center">
-      <router-link to="/main" class="underline">Get Random Stretch</router-link>
-      <span>•</span>
-      <a href="mailto:feedback@koljapluemer.com" class="underline">Give Feedback</a>
-      <span>•</span>
-      More on <a class="underline" href="https://koljapluemer.com/">koljapluemer.com</a>
-      <span>•</span>
-      <router-link to="/about" class="underline">About</router-link>
-    </div>
+    <a href="mailto:feedback@koljapluemer.com" class="link">→ Give Feedback</a>
+    <span>
+      → More on <a class="link" href="https://koljapluemer.com/">koljapluemer.com</a>
+    </span>
+    <router-link to="/about" class="link">→ About</router-link>
   </footer>
 </template>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,28 +1,30 @@
 <template>
-  <img src="@/assets/workout.svg" alt="" class=" mx-auto" />
-  <ul class="text-lg text-left list-disc mt-10">
-    <li>You are convinced that some stretching is probably good.</li>
-    <li>
-      You are annoyed by endless, confusing articles, often incomplete, presuming previous
-      skills, sporting long intros...
-    </li>
-  </ul>
-  <div class="flex gap-2 flex-col">
-    <router-link class="btn btn-primary btn-lg" to="/main">
-      Get a Random Stretch Right Now
-    </router-link>
+  <article class="max-w-lg mx-auto">
+    <img src="@/assets/workout.svg" alt="" class="mx-auto" />
+    <ul class="text-lg text-left list-disc mt-10">
+      <li>You are convinced that some stretching is probably good.</li>
+      <li>
+        You are annoyed by endless, confusing articles, often incomplete, presuming previous
+        skills, sporting long intros...
+      </li>
+    </ul>
+    <div class="flex gap-2 flex-col mx-auto mt-10">
+      <router-link class="btn btn-primary btn-lg" to="/main">
+        Get a Random Stretch Right Now
+      </router-link>
 
-    <router-link class="btn btn-sm" :to="{ name: 'Main', query: { officeOnly: true } }"
-      :class="{ 'btn-primary': $route.query.officeOnly === 'true' }">
-      Office Stretch
-    </router-link>
+      <router-link class="btn btn-sm" :to="{ name: 'Main', query: { officeOnly: true } }"
+        :class="{ 'btn-primary': $route.query.officeOnly === 'true' }">
+        Office Stretch
+      </router-link>
 
-    <router-link class="btn btn-sm" :to="{ name: 'Main', query: { deskOnly: true } }"
-      :class="{ 'btn-primary': $route.query.deskOnly === 'true' }">
-      Desk Stretch
-    </router-link>
+      <router-link class="btn btn-sm" :to="{ name: 'Main', query: { deskOnly: true } }"
+        :class="{ 'btn-primary': $route.query.deskOnly === 'true' }">
+        Desk Stretch
+      </router-link>
 
-  </div>
+    </div>
+  </article>
 
 </template>
 
